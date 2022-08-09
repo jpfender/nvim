a82be73e7ecf76f001c45582175eb95153c35a67
Jakob Pfender <jakob.pfender@safetyio.com>
2022-08-09 11:08:14 +0200

    lint: Add cfn-lint

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -1,27 +1,37 @@
 -- CloudFormation
-require('lint').linters.cfn_lint = {
-    cmd = 'cfn-lint',
-    stdin = true,
+
+local cfn_lint_pattern = "([^:]+):(%d+):(%d+):(%d+):(%d+):(%w)(%d+):(.*)"
+local cfn_lint_groups = { "file", "lnum", "col", "end_lnum", "end_col", "severity", "code", "message" }
+local cfn_lint_severity_map = {
+	["W"] = vim.diagnostic.severity.WARN,
+	["E"] = vim.diagnostic.severity.ERROR,
 }
-require('lint').linters.cfn_nag_scan = {
-    cmd = 'cfn_nag_scan --input-path',
-    stdin = false,
+
+require("lint").linters.cfn_lint = {
+	cmd = "cfn-lint",
+	args = { "--format", "parseable" },
+	stdin = false,
+	parser = require("lint.parser").from_pattern(
+		cfn_lint_pattern,
+		cfn_lint_groups,
+		cfn_lint_severity_map,
+		{ ["source"] = "cfn-lint" }
+	),
 }
 
 -- Select linter by filetype
-require('lint').linters_by_ft = {
-    ansible = { 'ansible_lint' },
-    cloudformation = { 'cfn_lint', 'cfn_nag_scan' },
-    javascript = { 'eslint' },
-    lua = { 'luacheck' },
-    markdown = { 'vale' },
-    python = { 'pylint', 'flake8' },
-    yaml = { 'yamllint' },
+require("lint").linters_by_ft = {
+	cloudformation = { "cfn_lint" },
+	javascript = { "eslint" },
+	lua = { "luacheck" },
+	markdown = { "vale" },
+	python = { "pylint", "flake8" },
+	yaml = { "yamllint" },
 }
 
 -- Automatically lint after writing
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
+	callback = function()
+		require("lint").try_lint()
+	end,
 })
