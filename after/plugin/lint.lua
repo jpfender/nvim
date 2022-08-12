6f5bc350943a29f94f7a02863b6dbbc97367dbca
Jakob Pfender <jakob.pfender@safetyio.com>
2022-08-12 15:46:52 +0200

    Remove local config of cfn-lint parser as code is now merged into upstream plugin

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -1,24 +1,3 @@
--- CloudFormation
-
-local cfn_lint_pattern = "([^:]+):(%d+):(%d+):(%d+):(%d+):(%w)(%d+):(.*)"
-local cfn_lint_groups = { "file", "lnum", "col", "end_lnum", "end_col", "severity", "code", "message" }
-local cfn_lint_severity_map = {
-	["W"] = vim.diagnostic.severity.WARN,
-	["E"] = vim.diagnostic.severity.ERROR,
-}
-
-require("lint").linters.cfn_lint = {
-	cmd = "cfn-lint",
-	args = { "--format", "parseable" },
-	stdin = false,
-	parser = require("lint.parser").from_pattern(
-		cfn_lint_pattern,
-		cfn_lint_groups,
-		cfn_lint_severity_map,
-		{ ["source"] = "cfn-lint" }
-	),
-}
-
 -- Select linter by filetype
 require("lint").linters_by_ft = {
 	cloudformation = { "cfn_lint" },
