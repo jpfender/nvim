3a9d0097286a64f42e669aae869539fd218c3ab9
Jakob Pfender <jakob.pfender@safetyio.com>
2022-09-01 12:04:30 +0200

    treesitter: add some parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,13 +1,34 @@
-require("nvim-treesitter.configs").setup {
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  }
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"bash",
+		"dockerfile",
+		"fish",
+		"gitignore",
+		"help",
+		"javascript",
+		"jsdoc",
+		"json",
+		"lua",
+		"make",
+		"markdown",
+		"python",
+		"rust",
+		"sql",
+		"typescript",
+		"vim",
+		"yaml",
+	},
+	-- Automatically install missing parsers when entering buffer
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	},
+})
