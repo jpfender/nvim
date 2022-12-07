7aeaab7dc64c3b051b743e7b87afbb312521168a
Jakob Pfender <Jakob.Pfender@MSASafety.com>
2022-12-07 14:50:04 +0100

    lualine: use rounded separators

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -18,8 +18,8 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "jellybeans",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = ")", right = "(" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
