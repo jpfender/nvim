30a952ada70daceb348f64611981ea1199611608
Jakob Pfender <jakob.pfender@safetyio.com>
2023-10-26 08:21:52 +0200

    add comment

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.ui.edgy" },
+    { import = "lazyvim.plugins.extras.ui.edgy" }, -- TODO: Figure out how to disable animation
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.leap" },
