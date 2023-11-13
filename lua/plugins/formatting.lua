db77e4a1fbb4d1a2bf7fbf7052d3744e9bd3d726
Jakob Pfender <jakob.pfender@safetyio.com>
2023-11-13 10:05:58 +0100

    Rely on prettier plugin to format md

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,7 +4,6 @@ return {
     opts = function(_, opts)
       opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft, {
         lua = { "stylua" },
-        markdown = { "markdownlint" },
         python = { "isort", "black" }, -- TODO: Revisit ruff
         sh = { "shfmt" },
         terraform = { "terraform_fmt" },
