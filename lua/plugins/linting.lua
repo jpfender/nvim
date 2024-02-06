76bdd064149b6a82fdd185cb90b2a7b39476cee6
Jakob Pfender <jakob.pfender@safetyio.com>
2024-02-06 10:14:17 +0100

    Switch back to upstream nvim-lint

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "nvim-lint",
+    "mfussenegger/nvim-lint",
     opts = function(_, opts)
       opts.linters_by_ft = vim.tbl_extend("force", opts.linters_by_ft, {
         ansible = { "ansible_lint" },
