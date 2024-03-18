ad6450558c2d0935aa5f0b868b44b8a919c5862a
Jakob Pfender <jakob.pfender@safetyio.com>
2024-03-18 10:40:58 +0100

    replace aerial with outline

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,8 +13,8 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.ui.edgy" }, -- TODO: Figure out how to disable animation
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.leap" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.ansible" },
