e59736de5f139a79f8cd261a31c799075805b4f8
Jakob Pfender <jakob.pfender@safetyio.com>
2025-05-19 12:40:53 +0200

    add cloudformation yaml Equals tag

--- lua/plugins/yaml.lua
+++ lua/plugins/yaml.lua
@@ -7,6 +7,7 @@ return {
           settings = {
             yaml = {
               customTags = {
+                "!Equals",
                 "!GetAtt",
                 "!If",
                 "!Ref",
